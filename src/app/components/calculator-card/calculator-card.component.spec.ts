4db1dfc3bfe35f9576cee53b150753e7a69187db
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-30 21:25:21 +0800

    Initialized.

---
+++ src/app/components/calculator-card/calculator-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalculatorCardComponent } from './calculator-card.component';
+
+describe('CalculatorCardComponent', () => {
+  let component: CalculatorCardComponent;
+  let fixture: ComponentFixture<CalculatorCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CalculatorCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CalculatorCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/calculator-card/calculator-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-card',
+  templateUrl: './calculator-card.component.html',
+  styleUrls: ['./calculator-card.component.scss']
+})
+export class CalculatorCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
