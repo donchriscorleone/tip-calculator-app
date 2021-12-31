2feec1d9e473256c28617d4d66c5d4fa85507bff
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-31 12:36:59 +0800

    Added functionality

--- src/app/components/calculator-card/calculator-card.component.ts
+++ src/app/components/calculator-card/calculator-card.component.ts
@@ -9,6 +9,10 @@ import { FormControl, Validators } from '@angular/forms';
 export class CalculatorCardComponent implements OnInit {
   bill: FormControl = new FormControl(null, [Validators.required, Validators.min(1)])
   numberOfPeople = new FormControl(null, [Validators.required, Validators.min(1)])
+  custom = new FormControl("Custom", [Validators.required, Validators.min(1)])
+  tip = 0;
+  tipAmount = 0;
+  total = 0;
 
   constructor() { }
 
@@ -19,4 +23,24 @@ export class CalculatorCardComponent implements OnInit {
     return (control.invalid && control.touched);
   }
 
+  changeTip(value: number) {
+    this.tip = value;
+    this.calculateTip();
+  }
+
+  calculateTip() {
+    this.tipAmount = parseFloat(((this.bill.value * this.tip) / this.numberOfPeople.value ).toFixed(2));
+    
+    this.total = parseFloat((this.tipAmount * this.numberOfPeople.value).toFixed(2));
+  }
+
+  reset() {
+    this.bill.reset();
+    this.numberOfPeople.reset();
+    this.custom.reset();
+    this.custom.setValue('Custom')
+    this.tip = 0;
+    this.tipAmount = 0;
+    this.total = 0;
+  }
 }
