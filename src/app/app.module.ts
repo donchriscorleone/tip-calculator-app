841e9912e3f98b212d9f1af734302e1e89db4869
Christopher II Lajom <christopheriilajom@gmail.com>
2021-12-31 11:54:55 +0800

    Added desktop view.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,7 +13,8 @@ import { CalculatorCardComponent } from './components/calculator-card/calculator
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/calculator-card/calculator-card.component.ts
+++ src/app/components/calculator-card/calculator-card.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-calculator-card',
+  selector: 'calculator-card',
   templateUrl: './calculator-card.component.html',
   styleUrls: ['./calculator-card.component.scss']
 })
 export class CalculatorCardComponent implements OnInit {
+  bill: FormControl = new FormControl(null, [Validators.required, Validators.min(1)])
+  numberOfPeople = new FormControl(null, [Validators.required, Validators.min(1)])
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isError(control: FormControl) {
+    return (control.invalid && control.touched);
   }
 
 }
